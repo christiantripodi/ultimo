a0dd1d3427f2ae3d47af3580d3e135a4bb905739
christiantripodi <skl11@icloud.com>
2024-07-11 16:31:32 +0200

    navbar

---
+++ src/components/CustomNavbar.tsx
@@ -0,0 +1,20 @@
+import { Navbar, Container, Nav } from "react-bootstrap"
+import {Link} from 'react-router-dom'
+
+const CustomNavbar = () => {
+    return(
+        <Navbar bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src="../public/x.png " alt="logo" style={{width:"300px"}} />
+                    News
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Link className="nav-link" to="/">Home</Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default CustomNavbar
---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+const Home = () => {
+    return "ciao"
+
+}
+
+
+export default Home
